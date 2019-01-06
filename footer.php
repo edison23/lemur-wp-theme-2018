8ba2200bb261be5b4bdea6f54ec3dd4dc386408b
edison23 <klementdavid@gmail.com>
2019-01-06 15:00:02 +0100

    Added contact info to the footer

--- footer.php
+++ footer.php
@@ -11,6 +11,8 @@
 				<p class="copyright"> 
 					Tento portál běží na redakčním systému <a href="//wordpress.org" title="WordPress">WordPress</a> se šablonou Lemur 2018 založené na šabloně <a href="//html5blank.com" title="HTML5 Blank">HTML5 Blank</a>.
 				</p>
+				<p class="copyright">
+					Našli jste chybu? Máte námět na zlepšení obsahu, přístupnosti či vzhledu našeho portálu? Napište vývojářům e-mail na <a href="mailto:redakce@lemur.mu">redakce@lemur.mu</a> (záležitosti týkající se obsahu) nebo nám popište nalezený problém na <a href="https://github.com/edison23/lemur-wp-theme-2018/issues" target="_blank">Githubu</a> (záležitosti týkající se vzhledu, přístupnosti či funkcionality).
 				<!-- /copyright -->
 
 			</footer>
