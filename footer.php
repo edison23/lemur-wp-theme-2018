6d19024443705d5976e63f997e312ad4e821c5c9
edison23 <klementdavid@gmail.com>
2019-03-04 00:45:20 +0100

    fixed a wrong e-mail address, polish wording of the text in the footer

--- footer.php
+++ footer.php
@@ -12,7 +12,7 @@
 					Tento portál běží na redakčním systému <a href="//wordpress.org" title="WordPress">WordPress</a> se šablonou Lemur 2018 založené na šabloně <a href="//html5blank.com" title="HTML5 Blank">HTML5 Blank</a>.
 				</p>
 				<p class="copyright">
-					Našli jste chybu? Máte námět na zlepšení obsahu, přístupnosti či vzhledu našeho portálu? Napište vývojářům e-mail na <a href="mailto:redakce@lemur.mu">redakce@lemur.mu</a> (záležitosti týkající se obsahu) nebo nám popište nalezený problém na <a href="https://github.com/edison23/lemur-wp-theme-2018/issues" target="_blank">Githubu</a> (záležitosti týkající se vzhledu, přístupnosti či funkcionality).
+					Našli jste chybu? Máte námět na zlepšení obsahu, přístupnosti či vzhledu našeho portálu? Napište nám e-mail na <a href="mailto:redakce@lemurmu.cz">redakce@lemurmu.cz</a> (záležitosti týkající se obsahu) nebo popište nalezený problém v <a href="https://github.com/edison23/lemur-wp-theme-2018/issues" target="_blank">našem repozitáři na Githubu</a> (záležitosti týkající se vzhledu, přístupnosti či funkcionality).
 				<!-- /copyright -->
 
 			</footer>
